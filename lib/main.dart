4709e81a1a0724359bb07d575e27414bf30831ba
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-25 23:09:47 +0500

    class and object program added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
 
 void main() {
+  ///  NULL SAFETY
   String? name;
+  print(name); // null
 
-  print(name!.length); // null
+  /// EQUALITY OPERATORS
+  var a = 10;
+  var b = 10;
+  print(a==b);
 
 
+  /// Objects
+  User user1 = User();
+
+  user1.name = "Ismail";
+  user1.email = "ismail@gmail.com";
+  print(user1.name);
+  print(user1.email);
 }
+
+
+/// Classes and Objects
+
+
+class User {
+  String name = "";
+  String email = "";
+}
+
