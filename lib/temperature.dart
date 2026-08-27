ed6ebd0e3217bbfe5e8e7bc0b3e01897eccf5d74
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-27 23:58:29 +0500

    celsius added

--- lib/temperature.dart
+++ lib/temperature.dart
@@ -1,3 +1,14 @@
 class Temperature {
+    double _celsius;
+
+
+    Temperature(this._celsius);
+
+    double get fahrenheit => (_celsius * 9 / 5) + 32;
+
+}
+void main() {
+  Temperature temp = Temperature(25);
+  print(temp.fahrenheit);
 
 }
