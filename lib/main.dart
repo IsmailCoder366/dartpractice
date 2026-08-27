c8cbc2980509c5d4fea8c4f1fa1ac1c060ac69b6
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-27 23:32:07 +0500

    getter method added

--- lib/main.dart
+++ lib/main.dart
@@ -2,58 +2,24 @@
 import 'encapsulation.dart';
 
 void main() {
-  BankAccount account = BankAccount("bilal", 3000);
-
+  BankAccount account = BankAccount(10000);
   print(account.balance);
 
-  account.deposit(5000);
 
-  print(account.balance);
-
-  account.withdraw(3000);
-
-  print(account.balance);
-
-  account.withdraw(20000);
-
-  print(account.balance);
-
-  print("encapsulation added");
-
-  print(account.balance);
-  account.withdraw(1000);
-  print(account.balance);
 }
 
+
+
 class BankAccount {
-  String accountHolder;
   double _balance;
 
-  BankAccount(this.accountHolder, this._balance);
+  BankAccount(this._balance);
 
   double get balance => _balance;
 
-  void deposit(double amount) {
-    if (amount <= 0) {
-      print("Invalid amount");
-      return;
+  set balance(double amount) {
+    if (amount >= 0) {
+      _balance = amount;
     }
-
-    _balance += amount;
-  }
-
-  void withdraw(double amount) {
-    if (amount <= 0) {
-      print("Invalid amount");
-      print("add valid payment");
-      return;
-    }
-
-    if (amount > _balance) {
-      print("Insufficient balance");
-      return;
-    }
-
-    _balance -= amount;
   }
 }
