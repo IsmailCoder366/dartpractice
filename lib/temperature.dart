228e6cd798f347b0c4632c8f1b4b4f5e156d830c
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-28 23:58:26 +0500

    temp example updated

--- lib/temperature.dart
+++ lib/temperature.dart
@@ -1,13 +1,30 @@
 class Temperature {
-  double _celcius;
+  double _celsius;
 
 
-  Temperature(this._celcius);
+  Temperature(this._celsius);
+  double get celsius => _celsius;
 
-  double get fahrenheit => (_celcius * 9/5) + 32;
+  double get fahrenheit => (_celsius * 9/5) + 32;
+
+  set celsius(double value) {
+    if(value >= -273.5) {
+      _celsius = value;
+    }
+    else {
+      print("invalid value");
+    }
+  }
 }
 
 void main() {
   Temperature temp = Temperature(40);
+
+  print(temp.celsius);
+  print(temp.fahrenheit);
+
+  temp.celsius = 100;
+
+  print(temp._celsius);
   print(temp.fahrenheit);
 }
